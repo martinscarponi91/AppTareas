37710e663e3788886a6d4bc849f99755d4971d8d
martin <martin.scarponi.ms@gmail.com>
2023-04-24 16:25:48 -0300

    revision de codigo

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const { list } = require('./functions/list');
 const { find } = require ('./functions/find');
 const { edit } = require("./functions/edit");
 
-//console.log (list());
-//console.log(find('estudiar'));
+console.log (list());
+console.log(find('estudiar'));
 console.log(edit("estudiar","de vez en cuando los jueves"));
